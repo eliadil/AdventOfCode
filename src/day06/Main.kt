570ed364e89edd1266df86a52d30b9a5521c09c8
eliadil <eliadil.dev@gmail.com>
2015-12-07 16:46:19 +0100

    Day6 package, will finish up later

---
+++ src/day06/Main.kt
@@ -0,0 +1,18 @@
+package day06
+
+import java.io.File
+
+/**
+ * Created by eliadil on 2015-12-07.
+ */
+
+fun main(args: Array<String>) {
+
+
+    val twoDimArr = Array(1000, {BooleanArray(1000)})
+    val file = File("resources/day06")
+    file.forEachLine { line ->
+
+    }
+
+}
